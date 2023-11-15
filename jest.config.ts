cf15295985a10e6373d322e70d4d0d17b09b4395
Nestor Recinos <00083120@UCA.EDU.SV>
2023-11-14 21:34:13 -0600

    :wrench: Setup test config

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: './',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.service.ts',
+    '**/commons/helpers/*.{ts,js}',
+    '**/*.resolver.ts',
+  ],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  modulePaths: ['<rootDir>'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@Common/(.*)': '<rootDir>/src/commons/$1',
+    '@Config/(.*)': '<rootDir>/src/config/$1',
+    '@Auth/(.*)': '<rootDir>/src/auth/$1',
+    '@User/(.*)': '<rootDir>/src/user/$1',
+    '@Constants/(.*)': '<rootDir>/src/constants/$1',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
