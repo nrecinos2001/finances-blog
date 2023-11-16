8645e7c6d51e8def6e04deed23a2746b2403af1f
Nestor Recinos <00083120@UCA.EDU.SV>
2023-11-16 13:13:46 -0600

    :safety_vest: Update validations for mongo id

--- src/constants/dto/id-param.dto.ts
+++ src/constants/dto/id-param.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsMongoId, IsNotEmpty, IsString } from 'class-validator';
 
 export class IdParamDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
+  @IsMongoId()
   id: string;
 }
