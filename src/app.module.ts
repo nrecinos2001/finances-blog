5c97fc87df8336f334a68a4ca2cc71f21fad7ed2
Nestor Recinos <00083120@UCA.EDU.SV>
2023-11-15 22:03:07 -0600

    :construction: Initialize post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModuleOptions } from '@Config/config.module';
 import { UserModule } from '@User/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [ConfigModuleOptions, UserModule, AuthModule],
+  imports: [ConfigModuleOptions, UserModule, AuthModule, PostModule],
   controllers: [],
   providers: [],
 })
