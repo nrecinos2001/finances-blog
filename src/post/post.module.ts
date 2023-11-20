11fbcffcfbb6ba698660fcf8e594424acac04dd7
Nestor Recinos <00083120@UCA.EDU.SV>
2023-11-19 21:52:43 -0600

    :sparkles: Add method to add or remove likes

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { PostService } from './services';
 import { PostController } from './controllers';
+import { LikesModule } from '@Likes/likes.module';
 
 @Module({
+  imports: [LikesModule],
   controllers: [PostController],
   providers: [PostService],
 })
