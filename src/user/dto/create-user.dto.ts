b57abb71e615607081807b25a0fa41b30dedd3b3
Nestor Recinos <00083120@UCA.EDU.SV>
2023-11-22 13:21:13 -0600

    :bug: Add validation for users

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,17 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @Transform(({ value }) => value?.trim())
   @IsString()
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty()
   @IsEmail()
+  @Transform(({ value }) => value?.trim())
   @IsString()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty()
+  @Transform(({ value }) => value?.trim())
   @IsString()
+  @IsNotEmpty()
   password: string;
 }
