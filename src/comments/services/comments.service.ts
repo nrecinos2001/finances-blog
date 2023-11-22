dde743d1eb3440194dd5311bb0719a277cef4739
Nestor Recinos <00083120@UCA.EDU.SV>
2023-11-21 22:11:34 -0600

    :sparkles: Create comments module and create comments function

---
+++ src/comments/services/comments.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+import { ILoggedUser } from '@Common/types';
+import { IdParamDto } from '@Constants/dto';
+import { PostService } from '@Post/services';
+
+import { CreateCommentDto } from '../dtos';
+import { commentsRepository } from '@Comments/repositories/comments.repository';
+
+@Injectable()
+export class CommentsService {
+  constructor(private readonly postService: PostService) { }
+  async create(
+    idParamDto: IdParamDto,
+    loggedUser: ILoggedUser,
+    createCommentDto: CreateCommentDto,
+  ) {
+    const { id: postId } = idParamDto;
+    const { id: userId } = loggedUser;
+    // Validate existing post
+    await this.postService.findOne(postId);
+    // save comment
+    await commentsRepository.create(postId, userId, createCommentDto);
+    const post = await this.postService.findOne(postId);
+    return post;
+  }
+}
